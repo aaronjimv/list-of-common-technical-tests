b229fb802bce4648d835004fdb620768a2f72276
Aaron Jimenez <aaronjimv@gmail.com>
2023-10-31 20:16:39 -0800

    palindrome.py added

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+"""
+Check is a word or sentence is a palindrome
+"""
+
+def palindrome(input):
+    original_input = input.replace(" ","")
+    output = original_input[::-1].replace(" ","")
+
+    if original_input == output:
+        print("Its a palindrome")
+    else:
+        print("Not a palindrome")
+
+if __name__ == '__main__':
+    palindrome_check = input("Enter a word: ")
+    palindrome(palindrome_check)
