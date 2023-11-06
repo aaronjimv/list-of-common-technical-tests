6cf71680dcf4d7e140945a6277b060b9a8703523
Aaron Jimenez <aaronjimv@gmail.com>
2023-11-05 16:33:38 -0800

    logic added to fibobacci.py

---
+++ fibonacci.py
@@ -0,0 +1,19 @@
+"""
+Fibonacci series
+"""
+
+def fibonnaci(n): # n -> number of series
+    fib_series = [0,1]
+
+    while len(fib_series) < n:
+        next_num = fib_series[-1] + fib_series[-2]
+        fib_series.append(next_num)
+
+    return fib_series
+
+if __name__ == '__main__':
+    n = int(input("Enter number of digits of the fibonacci series to calculate : "))
+    serie = fibonnaci(n)
+    print(f"Fibonacci serie of {n} is: ")
+    for s in serie:
+        print(s)
