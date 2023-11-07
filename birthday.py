583a84be4be1d0df098555aa16dc46458b11f11a
Aaron Jimenez <aaronjimv@gmail.com>
2023-11-06 17:02:06 -0800

    logic of birthday.py added

--- birthday.py
+++ birthday.py
@@ -2,3 +2,16 @@
 Design an algorithm that determines the
 day you were born, based on the date
 """
+
+import datetime
+
+def birthday(birthday_date):
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    index_day = birthday_date.weekday()
+    return days[index_day]
+
+if __name__ == '__main__':
+    date_string = input("Enter your dirthday (YYY-MM-DD) : ")
+    date_object = datetime.datetime.strptime(date_string, "%Y-%m-%d")
+    day = birthday(date_object)
+    print(f"You was born a {day}")
