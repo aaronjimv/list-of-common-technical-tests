eb9b95fa01a67bba6f25e823765116ca9c671579
Aaron Jimenez <aaronjimv@gmail.com>
2023-11-13 16:50:11 -0800

    flake8 applieded to birthday.py

--- birthday.py
+++ birthday.py
@@ -5,10 +5,20 @@ day you were born, based on the date
 
 import datetime
 
+
 def birthday(birthday_date):
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    days = [
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday'
+    ]
     index_day = birthday_date.weekday()
     return days[index_day]
+
 
 if __name__ == '__main__':
     date_string = input("Enter your dirthday (YYY-MM-DD) : ")
