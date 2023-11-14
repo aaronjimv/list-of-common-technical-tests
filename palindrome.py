d4ff359681037a112ce61f0ee9237e44493a758c
Aaron Jimenez <aaronjimv@gmail.com>
2023-11-13 16:53:18 -0800

    flake8 applieded

--- palindrome.py
+++ palindrome.py
@@ -2,14 +2,16 @@
 Check is a word or sentence is a palindrome
 """
 
+
 def palindrome(input):
-    original_input = input.replace(" ","")
-    output = original_input[::-1].replace(" ","")
+    original_input = input.replace(" ", "")
+    output = original_input[::-1].replace(" ", "")
 
     if original_input == output:
         print("Its a palindrome")
     else:
         print("Not a palindrome")
+
 
 if __name__ == '__main__':
     palindrome_check = input("Enter a word: ")
