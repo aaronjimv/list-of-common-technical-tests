0e765a43cb50aa8692342593b94077083276f877
Aaron Jimenez <aaronjimv@gmail.com>
2023-11-13 16:51:59 -0800

    flake8 applieded to fibonaci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,8 +2,9 @@
 Fibonacci series
 """
 
-def fibonnaci(n): # n -> number of series
-    fib_series = [0,1]
+
+def fibonnaci(n):  # n -> number of series
+    fib_series = [0, 1]
 
     while len(fib_series) < n:
         next_num = fib_series[-1] + fib_series[-2]
@@ -11,8 +12,11 @@ def fibonnaci(n): # n -> number of series
 
     return fib_series
 
+
 if __name__ == '__main__':
-    n = int(input("Enter number of digits of the fibonacci series to calculate : "))
+    n = int(input(
+        "Enter number of digits of the fibonacci series to calculate : "
+        ))
     serie = fibonnaci(n)
     print(f"Fibonacci serie of {n} is: ")
     for s in serie:
